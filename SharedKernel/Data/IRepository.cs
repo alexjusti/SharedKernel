b367e841f9df8f26487c3c55f891300488628bf8
Alex Justi <arjusti@outlook.com>
2020-11-06 20:34:03 -0600

    Add generic repository implementation

---
+++ SharedKernel/Data/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MongoDB.Entities;
+using SharedKernel.Common;
+using SharedKernel.Identity;
+
+namespace SharedKernel.Data
+{
+    public interface IRepository<TEntity>
+        where TEntity : Entity
+    {
+        Task<ActionResult<Entity>> AddAsync(TEntity entity);
+
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task<bool> ExistsAsync(string id);
+
+        Task<IEnumerable<Entity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task<TEntity> GetAsync(string id);
+
+        Task<ActionResult<TEntity>> UpdateAsync(TEntity entity);
+
+        Task<ActionResult> DeleteAsync(string id);
+    }
+}
