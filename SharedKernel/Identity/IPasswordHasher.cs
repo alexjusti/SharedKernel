15aad58c308a719bf656443bc6f9c88950aa506f
Alex Justi <arjusti@outlook.com>
2020-11-09 17:55:43 -0600

    Add documentation

--- SharedKernel/Identity/IPasswordHasher.cs
+++ SharedKernel/Identity/IPasswordHasher.cs
@@ -4,8 +4,19 @@ namespace SharedKernel.Identity
 {
     public interface IPasswordHasher
     {
+        /// <summary>
+        /// Generate a storable hash derived from a password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
         string HashPassword(string password);
 
+        /// <summary>
+        /// Verify a password attempt against a stored hash
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordAttempt"></param>
+        /// <returns></returns>
         bool VerifyPassword(string password, string passwordAttempt);
     }
 }
--- SharedKernel/Identity/IUserManager.cs
+++ SharedKernel/Identity/IUserManager.cs
@@ -8,24 +8,77 @@ namespace SharedKernel.Identity
     public interface IUserManager<TUser>
         where TUser : User
     {
+        /// <summary>
+        /// Add a new user to the database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         Task<ActionResult<TUser>> AddUserAsync(TUser user);
 
+        /// <summary>
+        /// Get a user's record by their user ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<ActionResult<TUser>> GetUserByIdAsync(string id);
 
+        /// <summary>
+        /// Get a user's record by their email address
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         Task<ActionResult<TUser>> GetUserByEmailAsync(string email);
 
+        /// <summary>
+        /// Get a user's record by their username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         Task<ActionResult<TUser>> GetUserByUsernameAsync(string username);
 
+        /// <summary>
+        /// Verify a user's password, obtaining their record by their user ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="passwordAttempt"></param>
+        /// <returns></returns>
         Task<ActionResult> VerifyUserPasswordByIdAsync(string id, string passwordAttempt);
 
+        /// <summary>
+        /// Verify a user's password, obtaining their record by their email address
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="passwordAttempt"></param>
+        /// <returns></returns>
         Task<ActionResult> VerifyUserPasswordByEmailAsync(string email, string passwordAttempt);
 
+        /// <summary>
+        /// Verify a user's password, obtaining their record by their username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="passwordAttempt"></param>
+        /// <returns></returns>
         Task<ActionResult> VerifyUserPasswordByUsernameAsync(string username, string passwordAttempt);
 
+        /// <summary>
+        /// Update a user's record
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         Task<ActionResult<TUser>> UpdateUserAsync(TUser user);
 
+        /// <summary>
+        /// Attempt to change the user's password
+        /// </summary>
+        /// <param name="changePasswordDto"></param>
+        /// <returns></returns>
         Task<ActionResult> ChangeUserPasswordAsync(ChangePasswordDto changePasswordDto);
 
+        /// <summary>
+        /// Delete a user from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<ActionResult> DeleteUserAsync(string id);
     }
 }
